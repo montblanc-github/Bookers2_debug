d2269bb81ddf9b38089113dc4012260b0e484b40
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-09 12:54:05 +0000

    [Add] レイアウト崩れ改善

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
