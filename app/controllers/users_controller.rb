2dc7664d5a3cc7c91d91fd982c6c6d466f1bfd8f
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-09 11:58:08 +0000

    [Add]起動エラー解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.all
     @book = Book.new
   end
 
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
